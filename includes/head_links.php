96c3dbd9274078457934c62f465ecb5eed54e45e
Mahak <mahakgarg8002@gmail.com>
2023-07-12 12:24:09 +0530

    css

---
+++ includes/head_links.php
@@ -0,0 +1,4 @@
+<!-- CSS Files -->
+<link rel="stylesheet" href="CSS/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap-icons/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="CSS/styles.css">
