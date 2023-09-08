1d23ede69f30e99929204adda3f8d3da86fefe2e
Mahak <mahakgarg8002@gmail.com>
2023-09-08 17:20:44 +0530

    About Section

--- includes/head_links.php
+++
@@ -1,4 +0,0 @@
-<!-- CSS Files -->
-<link rel="stylesheet" href="CSS/bootstrap.min.css">
-<link rel="stylesheet" href="bootstrap-icons/font/bootstrap-icons.min.css">
-<link rel="stylesheet" href="CSS/styles.css">
